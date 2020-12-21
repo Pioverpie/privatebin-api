579bc6142926ab16d4eead5cf60274743e90c3d6
Pioverpie <pioverpie@gmail.com>
2020-12-20 22:22:31 -0500

    Remove offline host from tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@ SERVERS_AND_FILES = (
     ('https://vim.cx', FILE),  # PrivateBin 1.3
     ('https://privatebin.gittermann1.de/', FILE),  # PrivateBin 1.2
     ('https://paste.carrade.eu/', FILE),  # PrivateBin 1.1
-    ('https://paste.nikul.in/', None)  # PrivateBin 1.0
+    # ('https://paste.nikul.in/', None),  # PrivateBin 1.0 (Host offline)
 )
 
 __all__ = ('MESSAGE', 'RESPONSE_DATA', 'SERVERS_AND_FILES')
